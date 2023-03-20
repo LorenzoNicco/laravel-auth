6a52a142704e538c444c3790a692a1b2561feb1c
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-20 19:13:59 +0100

    Add seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,13 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// models
+use App\Models\Project;
+
+// utilities
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 class ProjectSeeder extends Seeder
 {
     /**
@@ -12,8 +19,15 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) {
+            $title = $faker->unique()->word();
+            Project::create([
+                'title' => $title,
+                'slug' => Str::slug($title),
+                'description' => $faker->paragraph()
+            ]);
+        }
     }
 }
